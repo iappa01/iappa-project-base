f669e1e362caa6fb8aa0b5579603523b3db7ab3b
LeTuanVu-Dev <letuanvu425@gmail.com>
2023-06-08 22:59:58 +0700

    ab

--- base-project/src/main/java/com/mobiai/base/ui/activity/BaseActivity.kt
+++ base-project/src/main/java/com/mobiai/base/ui/activity/BaseActivity.kt
@@ -17,8 +17,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import com.mobiai.R
-import com.mobiai.base.language.LanguageUtil
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.CompositeDisposable
