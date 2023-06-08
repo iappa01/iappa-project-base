bfe3522803dec2622600ae1eb1dfc78d624db0ef
LeTuanVu-Dev <letuanvu425@gmail.com>
2023-06-08 22:22:03 +0700

    ab

--- app/src/main/java/com/mobiai/base/storage/StorageCommon.kt
+++
@@ -1,9 +0,0 @@
-package com.mobiai.base.storage
-
-import androidx.lifecycle.MutableLiveData
-import com.ads.control.ads.wrapper.ApNativeAd
-
-class StorageCommon {
-    var nativeAdLanguage = MutableLiveData<ApNativeAd>()
-
-}
